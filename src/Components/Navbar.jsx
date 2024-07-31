9a99f4498c788c9cb940fdd02694ac9b7d3a74cd
joaojcfr <joao.ferreira95@yahoo.com.br>
2024-07-31 19:05:20 +0000

    css, body and bootstrap changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,14 +3,22 @@ import'../index.css'
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className='fixed top-0 w-full bg-blue-500 p-4'>
-        <ul className='flex justify-around'>
-          <li className='text-white'>Home</li>
-          <li className='text-white'>About</li>
-          <li className='text-white'>Contact</li>
-        </ul>
-      </nav>
+    <div className="nav top d-flex">
+      <div className="top_row_left">
+        
+        <img src="https://placehold.co/200x150" alt="" />
+
+      </div>
+      <div className="top_row_middle">
+        <div className="top_middle">
+            Home | Contact | Rate
+        </div>
+        <div className="bottom_middle">
+            <h1>Delivery Express</h1>
+        </div>
+        
+        </div>
+      <div className="top_row_right">Login | Sign Up!</div>
     </div>
   )
 }
