6d4bddf057017fb35afd2b321135d27f112f08e5
joaojcfr <joao.ferreira95@yahoo.com.br>
2024-08-09 18:35:06 +0000

    created new css modules, worked on pages login and contact. finished Navbar

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import styles from './Login.module.css'
 
 export const Login = () => {
 
-
-
-
+  const handlesubmit = (message) => {
+      console.log(message)
+  }
     
   return (
-    <div>Login</div>
-  )
+    <div className={styles.main_frame}>
+        <div className="frame_top">
+            <h1>Login</h1><br></br>
+        </div>
+    
+        <form action={handlesubmit("my message")}>
+            <div className="">
+                <div className="">
+                    <label for="email@gmail.com" className="form-label">Username</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                </div>
+            </div>
+            <div className="">
+                <div className="">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                </div>
+                <br></br>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+                
+    
+    
+        </form>
+    </div>
+    )
 }
+
+export default Login;
